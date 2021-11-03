82ec2f50223ce31aaa611150d703453498dd39e1
PandA <1554296563@qq.com>
2021-11-03 20:51:42 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Labels.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Labels.vue</p>
+    </div>
     <Nav></Nav>
   </div>
 </template>
@@ -11,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
