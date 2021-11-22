e958948ab1ac44f49d0edff23a0461b081aa8d85
PandA <1554296563@qq.com>
2021-11-22 23:17:52 +0800

    可以编辑和删除标签

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"></Icon>
+      <Icon class="leftIcon" name="left" @click="goBack"></Icon>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"></FormItem>
+      <FormItem :value="tag.name" @update:value="update" field-name="标签名" placeholder="请输入标签名"></FormItem>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -20,27 +20,44 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
+
 @Component({
   components: {Button, FormItem: FormItem}
 })
 export default class EditLabel extends Vue {
-  tag?:{id:string,name:string} = undefined
-  created(){
+  tag?: { id: string, name: string } = undefined;
+
+  created() {
     const id = this.$route.params.id;
     tagListModel.fetch;
     const tags = tagListModel.data;
-    const tag = tags.filter(t=>t.id===id)[0];
-    if(tag){
-      this.tag = tag
-    }else{
-      this.$router.replace('/404')
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      this.tag = tag;
+    } else {
+      this.$router.replace('/404');
     }
+  }
+
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
+    }
+  }
+  goBack(){
+    this.$router.back();
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.navBar{
+.navBar {
   text-align: center;
   font-size: 16px;
   padding: 12px 16px;
@@ -48,22 +65,27 @@ export default class EditLabel extends Vue {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  >.title{
+
+  > .title {
   }
-  >.leftIcon{
+
+  > .leftIcon {
     width: 24px;
     height: 24px;
   }
-  >.rightIcon{
+
+  > .rightIcon {
     width: 24px;
     height: 24px;
   }
 }
-.form-wrapper{
+
+.form-wrapper {
   background: white;
   margin-top: 8px;
 }
-.button-wrapper{
+
+.button-wrapper {
   text-align: center;
   padding: 16px;
   margin-top: 44-16px;
