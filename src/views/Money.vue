49051139b9b0341044cfb85424dc65f7b6c10df4
PandA <1554296563@qq.com>
2021-11-25 13:23:05 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,8 +15,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
 
 
@@ -26,7 +25,7 @@ import recordListModel from '@/models/recordListModel';
 export default class Money extends Vue {
   tags = window.tagList;
   // eslint-disable-next-line no-undef
-  recordList: RecordItem[] = recordListModel.fetch();
+  recordList = window.recordList;
   // eslint-disable-next-line no-undef
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
@@ -43,13 +42,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record);
+    window.createRecord(this.record);
   }
 
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
 }
 </script>
 
